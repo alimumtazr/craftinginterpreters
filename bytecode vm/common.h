92e763e35d6e1dc24d27fa403128aa1727a4a8c5
alimumtazr <u2023559@giki.edu.pk>
2026-06-21 16:11:01 +0500

    genesis

---
+++ bytecode vm/common.h
@@ -0,0 +1,16 @@
+#ifndef clox_common_h
+#define clox_common_h
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#define UINT8_COUNT (UINT8_MAX + 1)
+
+// Uncomment to trace bytecode execution.
+// #define DEBUG_TRACE_EXECUTION
+
+// Uncomment to disassemble compiled bytecode.
+// #define DEBUG_PRINT_CODE
+
+#endif
